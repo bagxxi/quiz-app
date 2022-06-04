3297e96fe82c7f463eef244cd2ae0390bd9950b7
Gabriel Balbontin <bagxxi@gmail.com>
2022-06-04 00:54:41 -0400

    Entrega desafío
    Archivos añadidos al proyecto python

---
+++ questions.py
@@ -0,0 +1,60 @@
+preguntas_basicas = {
+    'pregunta_1': {'enunciado': ['¿En qué año el hombre pisó la Luna por primera vez?'],
+                   'alternativas': [['1979', 0],
+                                    ['1969', 1],
+                                    ['1958', 0],
+                                    ['1964', 0]]},
+    'pregunta_2': {'enunciado': ['¿Quiénes fueron, según la leyenda, los dos hermanos fundadores de la ciudad de Roma?'],
+                   'alternativas': [['René y Romo', 0],
+                                    ['Rómulo y Remo', 1],
+                                    ['Rino y Reno', 0],
+                                    ['Roma y Remo', 0]]},
+
+    'pregunta_3': {'enunciado': ['¿Cuál es el monte más alto del mundo?'],
+                   'alternativas': [['Ojos del Nevado', 0],
+                                    ['Everest', 1],
+                                    ['Kilimanjaro', 0],
+                                    ['Torre Eiffel', 0]]}
+}
+
+preguntas_intermedias = {
+    'pregunta_1': {'enunciado': ['¿Cuál es el país de mayor tamaño del mundo?'],
+                   'alternativas': [['China', 0],
+                                    ['Rusia', 1],
+                                    ['Antártica', 0],
+                                    ['Brasil', 0]]},
+    'pregunta_2': {'enunciado': ['¿Cómo se llama la madre de Simba en la película de Disney “El Rey León”?'],
+                   'alternativas': [['Nala', 0],
+                                    ['Sarabi', 1],
+                                    ['Habibi', 0],
+                                    ['Levi', 0]]},
+
+    'pregunta_3': {'enunciado': ['¿Por cuántos estados está formado Estados Unidos?'],
+                   'alternativas': [['51', 0],
+                                    ['50', 1],
+                                    ['52', 0],
+                                    ['49', 0]]}
+}
+
+preguntas_avanzadas = {
+    'pregunta_1': {'enunciado': ['¿Quién pintó el “Guernica”?'],
+                   'alternativas': [['Dalí', 0],
+                                    ['Picasso', 1],
+                                    ['Van Gogh', 0],
+                                    ['Matisse', 0]]},
+    'pregunta_2': {'enunciado': ['¿Quién fue la primer persona en recibir dos Premios Nobel en distintas especialidades?'],
+                   'alternativas': [['Albert Einstein', 0],
+                                    ['Marie Curie', 1],
+                                    ['Gabriela Mistral', 0],
+                                    ['Sir Alexander Fleming', 0]]},
+
+    'pregunta_3': {'enunciado': ['¿En qué mes celebran los rusos la Revolución de Octubre?'],
+                   'alternativas': [['Octubre', 0],
+                                    ['Noviembre', 1],
+                                    ['Septiembre', 0],
+                                    ['Mayo', 0]]}
+}
+
+pool_preguntas = {'basicas': preguntas_basicas,
+                  'intermedias': preguntas_intermedias,
+                  'avanzadas': preguntas_avanzadas}
